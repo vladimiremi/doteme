59c71ae42405971f2ef9a870a930ad473f8a52ce
vladimiremi <vladimiremi2014@gmail.com>
2020-08-03 22:57:27 -0300

    Layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import  logo  from './assets/logo.png';
+import main from './assets/Main.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img className="logo" src={logo} alt=""/>
+        <nav>
+          <ul>
+            <li>
+              Home
+            </li>
+            <li>
+              About
+            </li>
+            <li>
+              Pets
+            </li>
+            <li>
+              Contact
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <div>
+        <h1>Adotp a pet and be happier! Some dog or cat...</h1>
+          <div className="but">
+          <button className="but1">Busque pets</button>
+          <button className="but2">Adote pets</button>
+          </div>
+        
+        </div>
+        <img className="main" src={main} alt="main-doteme"/>
+
+      </main>
+
+
+      <footer>
+
+      </footer>
+
+
+
+
+
+
+
+
     </div>
   );
+
 }
 
 export default App;
